10d27081a6078c20e038751a9684bfd618739172
sniffjoke <sniffjoke@gmail.com>
2024-07-14 19:38:15 +0200

    commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,7 @@ function App() {
         >
           Learn React
         </a>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
-        <p>5</p>
+        <p>11</p>
       </header>
     </div>
   );
